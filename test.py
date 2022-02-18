bf210433fd99e3c733c3781ca679d1404d043c67
Daniel Munteanu <88230635+Micsuit@users.noreply.github.com>
2022-02-18 10:28:31 +0000

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import timeit
-
-def not_not_conv(x): return not not x
-
-def bool_conv(x): return bool(x)
-
-print(f"bool: {timeit.timeit('def bool_conv(x): return bool(x)')} sec\nnot not: {timeit.timeit('def not_not_conv(x): return not not x')} sec")
