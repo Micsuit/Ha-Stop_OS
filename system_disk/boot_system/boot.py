ce574c128cabdc1ed8b68d34df262e5e9d2b8903
Daniel Munteanu <88230635+Micsuit@users.noreply.github.com>
2022-02-28 14:48:55 +0000

    Update boot.py

--- system_disk/boot_system/boot.py
+++ system_disk/boot_system/boot.py
@@ -8,6 +8,15 @@ def test_ram():
 def test_cpu():
     test_all = [PC_CPU.name == MIN_CPU.name, 
                 PC_CPU.bits >= MIN_CPU.bits,
+from errors.system_error import *
+from sysINF.SYS_INF import *
+
+def test_ram():
+   return PC_RAM.size >= MIN_RAM.size
+
+def test_cpu():
+    test_all = [PC_CPU.name == MIN_CPU.name, 
+                PC_CPU.bits >= MIN_CPU.bits,
                 PC_CPU.power >= MIN_CPU.power]
 
     return all(test_all)
@@ -17,30 +26,19 @@ def test_storage():
     
 
 def test_comp():
-    test_all_comp = [test_ram(), test_cpu(), test_storage()]
-
-    return all(test_all_comp)
+    return all([test_ram(), test_cpu(), test_storage()])
 
 def boot_sys():
     error = None
-    #try:
-    #    print(os.getcwd())
-    #    os.chdir("REAL_SYSTEM_DISK_FOLDER")
-    #except FileNotFoundError:
-    #    error = DiskNotFoundInOS()
-    #    return "Boot Failed", error.details
 
     print("Testing Components...")
     test_comp_boot = test_comp()
-    if test_comp_boot:
-        print("Test Sucessful")
+    if test_comp_boot: print("Test Sucessful")
     else:
         error = PCCantRunSys()
         print("Test Failed")
         return "Boot Failed", error.details
 
     return "Boot Sucessful", error
-
-
 
     
