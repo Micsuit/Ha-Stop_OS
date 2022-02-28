c72b12fe0b032cbdcc32c58ca12476e0f0bf11d5
Daniel Munteanu <88230635+Micsuit@users.noreply.github.com>
2022-02-28 13:50:57 +0000

    Update command_list.py

--- system_disk/main_apps/command_list.py
+++ system_disk/main_apps/command_list.py
@@ -1,6 +1,7 @@
 import sys, time, os
-from sysINF import SYS_INF
+from sysINF.SYS_INF import *
 from errors.system_error import *
+import time
 import time
 
 SHUTDOWN = "shutdown" # shutdown the system
@@ -13,10 +14,25 @@ FILESINFOLDER = "filfol" # make a list of files (and folders) in the current fol
 WRITESCREEN = "writescrn" # write a message in the screen, "echo" in cmd
 OPEN = "open" # open a file
 GETSTATSPC = "statspc" # get stats from pc, like ram, cpu and etc...
-DATETIME = "datime"
+DATETIME = "datime" # Get current date and time
+COPY = "copy" # copy files and folders to another directories (probably is only being added in a future ver)
+HELP = "help" # Get commands available
+
+cmd_help = {SHUTDOWN: "Shutdown the system",
+            GOTO: "Go to a folder",
+            CREATEFILE: "Create a file",
+            CREATEFOLDER: "Create a folder", 
+            DELETEFILE: "Delete a folder", 
+            FILESINFOLDER: "Make a list of files and folders in the current folder",
+            WRITESCREEN: "Write a message on to the screen",
+            OPEN + " (to do)": "Open a file",
+            GETSTATSPC: "Get stats from pc, like RAM, CPU...",
+            DATETIME: "Get current date and time",
+            COPY + " (to do)": "Copy files or folders to another directories",
+            HELP: "Get all commands available"}
 
 def shutdown_cm():
-    print(f"Turning off {SYS_INF.SYSTEM_NAME}...")
+    print(f"Turning off {SYSTEM_NAME}...")
     time.sleep(3)
     sys.exit()
     
@@ -48,7 +64,7 @@ def createfile_cm(file):
 
 def createfolder_cm(folder):
     error = None
-    if os.path.isdir(folder):
+    if os.path.isdir(folder) or os.path.isfile(folder):
         error = FolderAlreadyExists(folder)
     else:
         os.mkdir(folder)
@@ -95,7 +111,7 @@ def filfol_cm(curr_folder):
     base_files_data = os.listdir()
     
     for flrfol in base_files_data:
-        flrfol_size = SYS_INF.type_stor.convert_show(os.path.getsize(flrfol))
+        flrfol_size = type_stor.convert_show(os.path.getsize(flrfol))
         flrfol_date = time.strftime('%d/%m/%Y', time.localtime(os.path.getmtime(flrfol)))
         
         if os.path.isfile(flrfol):
@@ -103,15 +119,33 @@ def filfol_cm(curr_folder):
         else:
             print(f"\n     - {flrfol} | {flrfol_date} | FOLDER")
 
-def writescreen_cm():
-    pass
-
-def open_file_cm():
-    pass
-
+def writescreen_cm(msg_to_wrt):
+    if msg_to_wrt:
+        print(msg_to_wrt)
+    else:
+        print("")
+    
 def getstatspc_cm():
-    pass
+    print("="*25 + " System Information " + "="*25)
+    print(f"""\n[System Parameters]\n\nSystem Name: {SYSTEM_NAME}\n\
+System Version: {SYSTEM_VER}\n\
+Year the system was created: {YEAR_MADE}\n\
+System Creator: {AUTHOR}\n\
+System Disk: {SYSTEM_DISK_FOLDER}\n\n[System Specs]\n\n\
+RAM: {PC_RAM}\n\
+CPU: {PC_CPU}\n\
+Storage: {SYS_STORAGE}\n""")
+    print("="*25 + " System Information " + "="*25)
 
 def datetime_cm():
+    print(f"Current date: {time.strftime('%d/%m/%Y %H:%M:%S')}")
+
+
+def help_cm():
+    print("List of all commands:")
+    for key, value in cmd_help.items():
+        print(f"\n  - {key.upper()}: {value}")
+
+def open_file_cm():
     pass
     
