42b972826e1bf04dad8d1407ea2230cb9c7769c1
Daniel Munteanu <88230635+Micsuit@users.noreply.github.com>
2022-02-28 13:51:59 +0000

    Update SYS_INF.py

--- system_disk/sysINF/SYS_INF.py
+++ system_disk/sysINF/SYS_INF.py
@@ -1,16 +1,16 @@
 class type_stor:
-    B = 1
-    KB = B * 1024
-    MB = KB * 1024
-    GB = MB * 1024
-    TB = GB * 1024
+    B = 1, "B"
+    KB = B * 1024, "KB"
+    MB = KB * 1024, "MB"
+    GB = MB * 1024, "GB"
+    TB = GB * 1024, "TB"
     
     
     def convert_byte(bytes, tpstor):
         return bytes / tpstor
     
     def convert_show(bytes):
-        lev_stor_str = {1: "B", 2: "KB", 3: "MB", 4: "GB", 5: "TB"}
+        lev_stor_str = {1: type_stor.B, 2: type_stor.KB, 3: type_stor.MB, 4: type_stor.GB, 5: type_stor.TB}
         converted_bytes = bytes
         i = 1
         
@@ -19,13 +19,13 @@ class type_stor:
             
             converted_bytes /= 1024
             
-        return f"{(round(converted_bytes, 2))} {lev_stor_str[i]}"
+        return f"{(round(converted_bytes, 2))} {lev_stor_str[i][1]}"
 
 
 class RAM:
     def __init__(self, num, type_storage):
         self.num = num
-        self.type_storage = type_storage
+        self.type_storage = type_storage[1]
         self.size = self.num * self.type_storage
 
     def __repr__(self):
@@ -38,12 +38,12 @@ class CPU:
         self.power = power
 
     def __repr__(self):
-        return [self.name, str(self.bits) + "-BITS", str(self.power) + " POWER"]
+        return f"{self.name}, {str(self.bits)}-BITS, {str(self.power)} POWER"
 
 class storage:
     def __init__(self, num, type_storage):
         self.num = num
-        self.type_storage = type_storage
+        self.type_storage = type_storage[1]
         self.size = self.num * self.type_storage
 
     def __repr__(self):
