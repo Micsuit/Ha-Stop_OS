d2cc941fb89e25ec7a2390ae42e015d935b81325
Daniel Munteanu <88230635+Micsuit@users.noreply.github.com>
2022-03-02 19:40:53 +0000

    updated version

--- system_disk/sysINF/SYS_INF.py
+++ system_disk/sysINF/SYS_INF.py
@@ -51,7 +51,7 @@ class storage:
 
 # [System Param]
 SYSTEM_NAME = "Ha-Stop OS"
-SYSTEM_VER = "v0.9.0-alpha"
+SYSTEM_VER = "v0.9.1-alpha"
 YEAR_MADE = 2022
 AUTHOR = "Daniel Munteanu"
 REAL_SYSTEM_DISK_FOLDER = "system_disk"
